608d55f80a49713c926aad4b021fba5fd9dd5253
bouromain <bouromain@gmail.com>
2021-04-13 21:18:52 +0100

    Speed optimisation for adjust_FOV_size.m

--- CellReg/adjust_FOV_size.m
+++ CellReg/adjust_FOV_size.m
@@ -17,6 +17,7 @@ number_of_sessions=size(spatial_footprints,2);
 adjusted_x_size=0;
 adjusted_y_size=0;
 FOV_all_sessions=cell(1,number_of_sessions);
+
 for n=1:number_of_sessions
     this_session_FOV=squeeze(spatial_footprints{n}(1,:,:));
     FOV_all_sessions{n}=ones(size(this_session_FOV,1),size(this_session_FOV,2));
@@ -39,18 +40,31 @@ for n=1:number_of_sessions
     new_adjusted_FOV(1:size(adjusted_FOV_temp,1),1:size(adjusted_FOV_temp,2))=adjusted_FOV_temp;
     adjusted_FOV_all_sessions(:,:,n)=new_adjusted_FOV;
     spatial_footprints_temp=spatial_footprints{n};
-    number_of_cells=size(spatial_footprints_temp,1);
-    adjusted_spatial_footprints_temp=zeros(number_of_cells,adjusted_y_size,adjusted_x_size);
-    for k=1:number_of_cells
-        unadjusted_spatial_footprint=squeeze(spatial_footprints_temp(k,:,:));
-        adjusted_unadjusted_spatial_footprint=zeros(adjusted_y_size,adjusted_x_size);
-        adjusted_unadjusted_spatial_footprint(1:size(unadjusted_spatial_footprint,1),1:size(unadjusted_spatial_footprint,2))=unadjusted_spatial_footprint;
-        adjusted_spatial_footprints_temp(k,:,:)=adjusted_unadjusted_spatial_footprint;
-    end
+    
+%     %% Old code
+%     number_of_cells=size(spatial_footprints_temp,1);
+%     adjusted_spatial_footprints_temp=zeros(number_of_cells,adjusted_y_size,adjusted_x_size);
+%     for k=1:number_of_cells
+%         unadjusted_spatial_footprint=squeeze(spatial_footprints_temp(k,:,:));
+%         adjusted_unadjusted_spatial_footprint=zeros(adjusted_y_size,adjusted_x_size);
+%         adjusted_unadjusted_spatial_footprint(1:size(unadjusted_spatial_footprint,1),1:size(unadjusted_spatial_footprint,2))=unadjusted_spatial_footprint;
+%         adjusted_spatial_footprints_temp(k,:,:)=adjusted_unadjusted_spatial_footprint;
+%     end
+% 
+%     adjusted_spatial_footprints2{n}=adjusted_spatial_footprints_temp;
+%     adjustment_zero_padding(1,n)=adjusted_x_size-size(unadjusted_spatial_footprint,2);
+%     adjustment_zero_padding(2,n)=adjusted_y_size-size(unadjusted_spatial_footprint,1);
+%     
+    %% New Code
+    [number_of_cells, sz_y, sz_x] = size(spatial_footprints_temp);
+    adjusted_spatial_footprints_temp = zeros(number_of_cells,adjusted_y_size,adjusted_x_size);
+    adjusted_spatial_footprints_temp(:,1:sz_y,1:sz_x) = spatial_footprints_temp;
+    
     adjusted_spatial_footprints{n}=adjusted_spatial_footprints_temp;
-    adjustment_zero_padding(1,n)=adjusted_x_size-size(unadjusted_spatial_footprint,2);
-    adjustment_zero_padding(2,n)=adjusted_y_size-size(unadjusted_spatial_footprint,1);
+    adjustment_zero_padding(1,n)=adjusted_x_size-sz_x;
+    adjustment_zero_padding(2,n)=adjusted_y_size-sz_y;
+    
+    % all(abs(adjusted_spatial_footprints{1}(:)-adjusted_spatial_footprints2{1}(:)) < eps)
 end
 
 end
-
