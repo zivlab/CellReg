feed5387b65cf1a548eaf9d22c2aa0047e84d32e
Sheintuch <lironsheintuch@gmail.com>
2022-01-26 18:16:08 +0200

    moved format converter to helper directory and updated README and log to new version v1.4.8.

--- Helper/format_conversion.m
+++ Helper/format_conversion.m
@@ -2,13 +2,21 @@
 
 input_format='Suite2p'; % could be either 'CNMF-e' or 'Suite2p'
 
-%% Choosing the files for conversion: 
+%% Choosing the files for conversion:
 [file_names,files_path]=uigetfile('*.mat','MultiSelect','on',...
-    'Choose spatial footprints from all the sessions: ' );
+    'Choose the spatial footprints from all the sessions: ' );
 
-number_of_sessions=size(file_names,2);
+if iscell(file_names)
+    number_of_sessions=size(file_names,2);
+else
+    number_of_sessions=1;
+end
 for n=1:number_of_sessions
-    this_session_data=load([files_path file_names{n}]);
+    if iscell(file_names)
+        this_session_data=load([files_path file_names{n}]);
+    else
+        this_session_data=load([files_path file_names]);
+    end
     if strcmp(input_format,'Suite2p')
         this_session_x_size=this_session_data.ops.Lx+1;
         this_session_y_size=this_session_data.ops.Ly+1;
@@ -29,6 +37,10 @@ for n=1:number_of_sessions
             this_session_converted_footprints(k,:,:)=reshape(this_session_data.neuron.A(:,k),this_session_y_size,this_session_x_size);
         end
     end
-    save([files_path 'converted_' file_names{n}],'this_session_converted_footprints','-v7.3')
+    if iscell(file_names)
+        save([files_path 'converted_' file_names{n}],'this_session_converted_footprints','-v7.3')
+    else
+        save([files_path 'converted_' file_names],'this_session_converted_footprints','-v7.3')
+    end
 end
 
