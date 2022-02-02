ed858618710c59a3b3163c358638fb44f829bfb3
Sheintuch <lironsheintuch@gmail.com>
2022-02-02 17:51:54 +0200

    edits to format conversion scripts.

--- Helper/format_conversion_inscopix.m
+++ Helper/format_conversion_inscopix.m
@@ -1,23 +1,15 @@
 %% Converting the Inscopix .tif cell spatial footprints to input format for CellReg
 
-clear all;
-
-input_format='Inscopix'; %
-
-%% Choosing the files for conversion:
-%[file_names,files_path]=uigetfile('*.mat','MultiSelect','on',...
-%    'Choose the spatial footprints from all the sessions: ' );
-[files_path]=uigetdir('Choose the location of the footprints: ' );
+[files_path]=uigetdir('Choose the location of the footprints from one session: ' );
 file_names = dir(fullfile(files_path, '**', '*_CNMFe_CellSet_C*'));
-
 %
 this_session_num_cells = length(file_names);
 
 % get size of footprints first
 fname = file_names(1);
-fname = fullfile(fname.folder, fname.name)
+fname = fullfile(fname.folder, fname.name);
 footprint = read(Tiff(fname,'r'));  
-
+[~,folder_name] = fileparts(files_path);
 %
 this_session_converted_footprints=zeros(this_session_num_cells,size(footprint, 1), size(footprint, 2));
 
@@ -32,6 +24,6 @@ for n=1:this_session_num_cells
     this_session_converted_footprints(n,:,:)= footprint;
     
 end
-fname_out = fullfile(file_names(1).folder, 'converted.mat');
+fname_out = fullfile(file_names(1).folder, [folder_name, '_converted.mat']);
 save(fname_out,'this_session_converted_footprints','-v7.3')
 
